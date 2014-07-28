8a2104f483beb88a61de8a2ce64d7fd85c27f6f5
miwara <aruim.jon@gmail.com>
2014-07-28 23:17:02 +0900

    ~/ -> HOME

--- placefiles.sh
+++ placefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-currentdir=~/dotfiles
+currentdir=$HOME/dotfiles
 
 if [ ! -e ./tmp ]; then
     mkdir ./tmp
@@ -8,13 +8,16 @@ fi
 cd ./tmp
 
 ln -s $currentdir/init.el init.el
-mv init.el ~/.emacs.d/
+mv init.el $HOME/.emacs.d/
 
 ln -s $currentdir/Cask Cask
-mv Cask ~/.emacs.d/Cask
+mv Cask $HOME/.emacs.d/Cask
+
+#ln -s $currentdir/.emacs.d .emacs.d
+#mv .emacs.d $HOME/
 
 ln -s $currentdir/.zshrc .zshrc
-mv .zshrc ~/
+mv .zshrc $HOME/
 
 ln -s $currentdir/.tmux.conf .tmux.conf
-mv .tmux.conf ~/
+mv .tmux.conf $HOME/
