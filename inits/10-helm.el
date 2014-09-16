6623788ee10d7af2b2850e1fac57bbe7a5935710
miuraK <miura@sencorp.co.jp>
2014-09-16 20:16:26 +0900

    tabで補完するように設定

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -3,3 +3,8 @@
 
 (require 'helm-config)
 (helm-mode 1)
+
+;; Tabで補完
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
