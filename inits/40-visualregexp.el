67ae322644febfe6cbc1e3215e74cf9660f4cba1
miuraK <miura@sencorp.co.jp>
2014-12-24 09:26:47 +0900

    visual-regexpをインストール

---
+++ inits/40-visualregexp.el
@@ -0,0 +1,4 @@
+(require 'visual-regexp)
+(define-key global-map (kbd "C-c q") 'vr/replace)
+(define-key global-map (kbd "C-c w") 'vr/query-replace)
+(define-key global-map (kbd "C-c e") 'vr/mc-mark)
