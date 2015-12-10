a2e38f940b2dca01f2e1d450722b1ca29cd20a07
miuraK <miura@sencorp.co.jp>
2015-12-11 02:08:52 +0900

    helm: seiri

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -3,8 +3,11 @@
 (require 'helm-config)
 (helm-mode 1)
 
-;; Tabで補完
+;; C-hで削除
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+
+;; Tabで補完
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
