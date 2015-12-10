4bffaf064adc4629a89e7c042554f8b61d7dcc2c
miuraK <miura@sencorp.co.jp>
2015-12-11 02:09:52 +0900

    helm: global map の設定

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -11,6 +11,12 @@
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
+(define-key global-map (kbd "M-x") 'helm-M-x)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-r") 'helm-recentf)
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
+
 ;; find-filesでタブを2回タイプするとバッファを開いてファイルの中身を見られるようにする
 (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
   "Execute command only of CANDIDATE exists"
