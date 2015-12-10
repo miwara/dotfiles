ac5ffe74d93d72d9bbee4a55da0ef1ff24fc5bac
miuraK <miura@sencorp.co.jp>
2015-12-11 01:12:28 +0900

    沈黙は金なり

--- placefiles.sh
+++ placefiles.sh
@@ -19,34 +19,28 @@ dotfiles=".zshrc .tmux.conf .gitignore .vimrc"
 
 for file in $dotemacsfiles
 do
-    echo "--- make $file link ---"
     if [ ! -e $dotemacsdir/$file ]; then
+        echo "--- make $file link ---"
         ln -s $currentdir/$file $file
         mv $file $dotemacsdir/$file
         echo "OK."
-    else
-        echo "File already exists."
-        echo "...skip"
+        echo "-----------------------"
+        echo ""
     fi
-    echo "-----------------------"
-    echo ""
 done
 
 for file in $dotfiles
 do
-    echo "--- make $file ---"
     if [ ! -e $HOME/$file ]; then
+        echo "--- make $file ---"
         ln -s $currentdir/$file $file
         mv $file $HOME/$file
         echo "OK."
-    else
-        echo "File already exists."
-        echo "...skip"
+        echo "-----------------------"
+        echo ""
     fi
-    echo "-----------------------"
-    echo ""
 done
 
 git config --global core.exculdesfile $HOME/.gitignore
 
-echo "\complete!/"
+echo "\done!/"
