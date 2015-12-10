4e18ba5964372218be132f45b2104d806827aaf9
miuraK <miura@sencorp.co.jp>
2015-12-11 01:42:00 +0900

    helm find-files でファイルの中身を確認するため

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -7,3 +7,9 @@
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; find-filesでタブを2回タイプするとバッファを開いてファイルの中身を見られるようにする
+(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+  "Execute command only of CANDIDATE exists"
+  (when (file-exists-p candidate)
+    ad-do-it))
