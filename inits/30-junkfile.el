ddcb6518194ca6aedbccece45923db9d8ede0f2b
miuraK <miura@sencorp.co.jp>
2015-12-11 23:14:24 +0900

    open-junk-file: path変更

--- inits/30-junkfile.el
+++ inits/30-junkfile.el
@@ -1,5 +1,5 @@
 ;; open-junk-file
 
 (require 'open-junk-file)
-(setq open-junk-file-format "~/Documents/programs/junk/%Y-%m-%d-%H%M%S.")
+(setq open-junk-file-format "~/Documents/junk/%Y-%m-%d-%H%M%S.")
 (global-set-key (kbd "C-x j") 'open-junk-file)
