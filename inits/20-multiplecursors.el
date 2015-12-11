f568e8416489878b24234b35f39e48b975d0c334
miuraK <miura@sencorp.co.jp>
2015-12-12 00:56:08 +0900

    multiple-cursors smartrep: これで使えたので

--- inits/20-multiplecursors.el
+++ inits/20-multiplecursors.el
@@ -2,7 +2,3 @@
 ;; https://github.com/emacsmirror/multiple-cursors
 
 (require 'multiple-cursors)
-
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
