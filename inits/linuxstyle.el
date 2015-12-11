b3f025f4904e67204278e458e5c9be8c3fccbfb7
miuraK <miura@sencorp.co.jp>
2015-12-11 18:02:29 +0900

    linux はこっちじゃなきゃフォント変えられなかった

--- inits/linuxstyle.el
+++ inits/linuxstyle.el
@@ -1,19 +1,19 @@
 ;; linux style
 
 ;; font
-;(add-to-list 'default-frame-alist '(font . "ricty-13.5"))
-(set-face-attribute 'default nil
-		    :family "Ricty Discord"
-		    :height 135)
-(set-fontset-font (frame-parameter nil 'font)
-		  'japanese-jisx0208
-		  (cons "Ricty Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-		  'japanese-jisx0212
-		  (cons "Ricty Discord" "iso10646-1"))
-(set-fontset-font (frame-parameter nil 'font)
-		  'katakana-jisx0201
-		  (cons "Ricty Discord" "iso10646-1"))
+(add-to-list 'default-frame-alist '(font . "ricty-13.5"))
+;(set-face-attribute 'default nil
+;		    :family "Ricty Discord"
+;		    :height 135)
+;(set-fontset-font (frame-parameter nil 'font)
+;		  'japanese-jisx0208
+;		  (cons "Ricty Discord" "iso10646-1"))
+;(set-fontset-font (frame-parameter nil 'font)
+;		  'japanese-jisx0212
+;		  (cons "Ricty Discord" "iso10646-1"))
+;(set-fontset-font (frame-parameter nil 'font)
+;		  'katakana-jisx0201
+;		  (cons "Ricty Discord" "iso10646-1"))
 
 ; THE QUICK BROWN FOX JUMPS OVER LAZY DOG.
 ; the quick brown fox jumps over lazy dog. 0123456789
