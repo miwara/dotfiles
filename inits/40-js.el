48839fe5cc26d06db2a360f993128a33beac166d
miuraK <miura@sencorp.co.jp>
2015-12-12 19:43:20 +0900

    js2-mode: ミスってた

--- inits/40-js.el
+++ inits/40-js.el
@@ -2,6 +2,5 @@
 
 (require 'js2-mode)
 
-(add-to-list 'auto-mode-alist '
-	     ("\\.js$" . js2-mode)
-	     ("\\.json$" . js2-mode)))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
