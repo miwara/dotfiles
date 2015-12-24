8b00b4d12503a054c6f8d963699608b0fef94b63
miuraK <miura@sencorp.co.jp>
2015-12-24 10:35:36 +0900

    js2-mode: インデントは2でソフトタブにする

--- inits/40-js.el
+++ inits/40-js.el
@@ -2,5 +2,11 @@
 
 (require 'js2-mode)
 
+(add-hook 'js2-mode-hook
+	  #'(lambda ()
+	      (setq js2-basic-offset 2
+		    indent-tabs-mode nil)
+	      ))
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
