0486fdae44649301a7ceb20dd0583fd64d0118bd
Kodai Miura <kodai.miura@emtg.co.jp>
2017-06-12 16:46:51 +0900

    いらない

--- inits/40-markdown.el
+++ inits/40-markdown.el
@@ -2,12 +2,8 @@
 
 ;;(require 'markdown-mode)
 
-;;(autoload 'markdown-mode "markdown-mode"
-;;  "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
-;;(autoload 'gfm-mode "markdown-mode"
-;;  "Major mode for editing GitHub Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("README\\.md$" . gfm-mode))
 
