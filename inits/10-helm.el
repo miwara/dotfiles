9cae82bca91d26e817c6a45d6d71d222766d59e9
Kodai Miura <kodai.miura@emtg.co.jp>
2018-06-27 19:43:54 +0900

    ローカルキーマップはこうする

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -7,17 +7,22 @@
 	 ("C-x C-f" . helm-find-files)
 	 ("C-x C-r" . helm-recentf)
 	 ("M-y" . helm-show-kill-ring)
-	 ("C-x b" . helm-buffers-list))
+	 ("C-x b" . helm-buffers-list)
+
+	 ;; C-hで削除
+	 :map helm-map
+	 ("C-h" . delete-backward-char)
+	 :map helm-find-files-map
+	 ("C-h" . delete-backward-char)
+
+	 ;; Tabで補完
+	 :map helm-find-files-map
+	 ([?\t] . helm-execute-persistent-action)
+	 :map helm-read-file-map
+	 ([?\t] . helm-execute-persistent-action)
+  )
 
   :config
-  ;; C-hで削除
-  (define-key helm-map (kbd "C-h") 'delete-backward-char)
-  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-
-  ;; Tabで補完
-  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
-
   ;; find-filesでタブを2回タイプするとバッファを開いてファイルの中身を見られるようにする
   (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
     "Execute command only of CANDIDATE exists"
