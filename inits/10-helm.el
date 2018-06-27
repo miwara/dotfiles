737790ef3fd4e27a2fa0b8fe49adfcc40ca4c2ce
Kodai Miura <kodai.miura@emtg.co.jp>
2018-06-27 19:33:20 +0900

    use-packgeで書き換えた

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -1,24 +1,26 @@
 ;; helm
 
-(require 'helm-config)
-(helm-mode 1)
+(use-package helm-config
+  :init (helm-mode t)
 
-;; C-hで削除
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("C-x C-r" . helm-recentf)
+	 ("M-y" . helm-show-kill-ring)
+	 ("C-x b" . helm-buffers-list))
 
-;; Tabで補完
-(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  :config
+  ;; C-hで削除
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 
-(define-key global-map (kbd "M-x") 'helm-M-x)
-(define-key global-map (kbd "C-x C-f") 'helm-find-files)
-(define-key global-map (kbd "C-x C-r") 'helm-recentf)
-(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
-(define-key global-map (kbd "C-x b") 'helm-buffers-list)
+  ;; Tabで補完
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
-;; find-filesでタブを2回タイプするとバッファを開いてファイルの中身を見られるようにする
-(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-  "Execute command only of CANDIDATE exists"
-  (when (file-exists-p candidate)
-    ad-do-it))
+  ;; find-filesでタブを2回タイプするとバッファを開いてファイルの中身を見られるようにする
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only of CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+)
