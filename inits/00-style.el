4a313361b6a747926da3440a5b252378193a0f8f
Kodai Miura <kodai.miura@emtg.co.jp>
2019-09-06 18:30:43 +0900

    マークをたどるのを簡単にするために

--- inits/00-style.el
+++ inits/00-style.el
@@ -76,6 +76,9 @@
 ;; 1行ずつスクロール
 (setq scroll-step 1)
 
+;; 過去のマークを C-u C-SPC C-SPC... と C-SPC の連打でたどる
+(setq set-mark-command-repeat-pop t)
+
 ;; Ctrl-Shift-k でカーソル位置から行頭まで削除
 (defun backward-kill-line (arg)
   "Kill chars backward until encoutering the end of a line"
