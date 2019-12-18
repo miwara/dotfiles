0e091dccf3c28524933506d3fa3cf814fb698403
Kodai Miura <kodai.miura@emtg.co.jp>
2019-12-18 02:34:06 +0000

    multiple-cursorsを使うにはkey-bindを設定する必要があった
    Note:
    "C->"，"C-<"で設定したかったができなかった

--- inits/20-multiplecursors.el
+++ inits/20-multiplecursors.el
@@ -2,4 +2,11 @@
 ;; https://github.com/emacsmirror/multiple-cursors
 
 (use-package multiple-cursors
-  :defer t)
+  :defer t
+
+  :bind (("C-^" . mc/edit-lines)
+	 ("M-[ 1 ; 6 n" . mc/mark-next-like-this)     ;; "C->" にしたかったがこれでないと動かなかった
+	 ("M-[ 1 ; 6 l" . mc/mark-previous-like-this) ;; "C-<" にしたかったがこれでないと動かなかった
+	 ("C-M-a" . mc/mark-all-like-this)
+  )
+)
