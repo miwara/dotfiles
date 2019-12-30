8c933ab619c38a99d3f59802350f51b5c5020d51
miwara <aruim.jon@gmail.com>
2019-12-31 00:51:48 +0900

    環境で分岐できなかったので

--- inits/20-multiplecursors.el
+++ inits/20-multiplecursors.el
@@ -5,8 +5,12 @@
   :defer t
 
   :bind (("C-^" . mc/edit-lines)
+	 ("C-M-a" . mc/mark-all-like-this)
+	 ;; windows用のkey-bind
 	 ("M-[ 1 ; 6 n" . mc/mark-next-like-this)     ;; "C->" にしたかったがこれでないと動かなかった
 	 ("M-[ 1 ; 6 l" . mc/mark-previous-like-this) ;; "C-<" にしたかったがこれでないと動かなかった
-	 ("C-M-a" . mc/mark-all-like-this)
+	 ;; mac用のkey-bind
+	 ("M->" . mc/mark-next-like-this)
+	 ("M-<" . mc/mark-previous-like-this)
   )
 )
