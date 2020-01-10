250f95b0fb7dba80a8c8dd37d643263bb5a7305b
miwara <aruim.jon@gmail.com>
2020-01-10 22:39:11 +0900

    競合していたので

--- inits/20-multiplecursors.el
+++ inits/20-multiplecursors.el
@@ -10,7 +10,7 @@
 	 ("M-[ 1 ; 6 n" . mc/mark-next-like-this)     ;; "C->" にしたかったがこれでないと動かなかった
 	 ("M-[ 1 ; 6 l" . mc/mark-previous-like-this) ;; "C-<" にしたかったがこれでないと動かなかった
 	 ;; mac用のkey-bind
-	 ("M->" . mc/mark-next-like-this)
-	 ("M-<" . mc/mark-previous-like-this)
+	 ("M-." . mc/mark-next-like-this)
+	 ("M-," . mc/mark-previous-like-this)
   )
 )
