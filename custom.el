c0a5fea8453d6450cab776f934fc2c85411be1a9
Kodai Miura <miura.kodai@tixplus.co.jp>
2020-12-28 18:19:53 +0900

    emacsが勝手にrefactorした

--- custom.el
+++ custom.el
@@ -7,8 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (w3m company use-package markdown-mode elixir-mode flycheck go-mode helm magit multiple-cursors open-junk-file php-mode popup popwin web-mode js2-mode yasnippet init-loader))))
+   '(w3m company use-package markdown-mode elixir-mode flycheck go-mode helm magit multiple-cursors open-junk-file php-mode popup popwin web-mode js2-mode yasnippet init-loader)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
