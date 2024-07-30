15cba35d320c51ca927e0c28f519b8b64bb87835
kmiura <koudai.miura@anykan.co.jp>
2024-07-30 16:24:35 +0900

    設定とパッケージのの整理

--- custom.el
+++ custom.el
@@ -6,8 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(w3m company use-package markdown-mode elixir-mode flycheck go-mode helm magit multiple-cursors open-junk-file php-mode popup popwin web-mode js2-mode yasnippet init-loader)))
+ '(package-selected-packages '(yasnippet multiple-cursors)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -19,12 +19,67 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; use-packageの設定
-(when (not (package-installed-p 'use-package))
-  (package-install 'use-package))
-(require 'use-package)
+(load "~/dotfiles/inits/00-style.el")
 
-;; init-loader
-(use-package init-loader
-	     :config
-	     (init-loader-load "~/dotfiles/inits"))
+(load "~/dotfiles/inits/00-system.el")
+
+;; -----------------------------------------------------------------------------
+
+;; Eshell
+(add-hook 'eshell-mode-hook (lambda ()
+			      (define-key eshell-mode-map "\C-a" 'eshell-bol)
+			      (setq eshell-scroll-show-maximum-output t)
+			      (setq eshell-scroll-to-bottom-on-output nil)
+			      (setq eshell-save-history-on-exit nil)
+			      (setq eshell-cmpl-ignore-case t)
+			      ))
+
+;; F6で起動
+(global-set-key [f6] 'eshell)
+
+;; eshell clear
+(defun eshell/clear ()
+  "Clear the current buffer, leaving one prompt at the top."
+  (interactive)
+  (let (inhibit-read-only t)
+    (erase-buffer)))
+
+
+;; yasnippet
+;; https://github.com/capitaomorte/yasnippet
+(use-package yasnippet
+  :ensure t
+  :init
+  (yas-global-mode t)
+
+  ;; 自作スニペットの保存先
+  (setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"))
+
+  :bind (:map yas-minor-mode-map
+	 ;; 既存スニペットを挿入する
+	 ("C-x i i" . yas-insert-snippet)
+	 ;; 新規スニペットを作成するバッファを用意する
+	 ("C-x i n" . yas-new-snippet)
+	 ;; 既存スニペットを閲覧・編集する
+	 ("C-x i v" . yas-visit-snippet-file)
+	)
+  )
+
+
+;; multiple-cursors
+;; https://github.com/emacsmirror/multiple-cursors
+(use-package multiple-cursors
+  :ensure t
+  :defer t
+
+  :bind (("C-^" . mc/edit-lines)
+	 ("C-M-a" . mc/mark-all-like-this)
+	 ;; windows用のkey-bind
+	 ("M-[ 1 ; 6 n" . mc/mark-next-like-this)     ;; "C->" にしたかったがこれでないと動かなかった
+	 ("M-[ 1 ; 6 l" . mc/mark-previous-like-this) ;; "C-<" にしたかったがこれでないと動かなかった
+	 ;; mac用のkey-bind
+	 ("M-." . mc/mark-next-like-this)
+	 ("M-," . mc/mark-previous-like-this)
+  )
+)
